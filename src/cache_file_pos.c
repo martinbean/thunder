0fbf4bdeb3d88bea2f659ac7c51b11a277c0f6dd
Martin Bean <martinbean@users.noreply.github.com>
2024-05-29 14:41:13 +0100

    Use typedef-d int type

--- src/cache_file_pos.c
+++ src/cache_file_pos.c
@@ -7,7 +7,7 @@ void *func_80021490(void*, uint32_t);
 
 void cache_file_pos(void)
 {
-	int i;
+	uint32_t i;
 
 	for (i = 0; i < sizeof(D_8004A4BC) / sizeof(D_8004A4BC[0]); i++) {
 		func_80021490(&D_8004A4BC[i], 1);
