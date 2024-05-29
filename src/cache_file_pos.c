60c04862ab17f02bff1530a05431bda94411a3ca
Martin Bean <martinbean@users.noreply.github.com>
2024-05-29 14:49:29 +0100

    Fix pointer placement

--- src/cache_file_pos.c
+++ src/cache_file_pos.c
@@ -3,7 +3,7 @@
 
 extern FileInfo D_8004A4BC[FILES_COUNT];
 
-extern void *func_80021490(FileInfo* files, uint32_t offset);
+extern void *func_80021490(FileInfo *files, uint32_t offset);
 
 void cache_file_pos(void)
 {
