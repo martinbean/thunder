fd0fb82ae9bf87ecab936c27c810abdcbe46de50
Martin Bean <martinbean@users.noreply.github.com>
2024-07-19 02:43:50 +0100

    Declare movie functions

---
+++ src/movie.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/movie", play_movie);
+
+INCLUDE_ASM("asm/nonmatchings/movie", strSetDefDecEnv);
+
+INCLUDE_ASM("asm/nonmatchings/movie", strInit);
+
+INCLUDE_ASM("asm/nonmatchings/movie", strCallback);
+
+INCLUDE_ASM("asm/nonmatchings/movie", strNextVlc);
+
+INCLUDE_ASM("asm/nonmatchings/movie", strNext);
+
+INCLUDE_ASM("asm/nonmatchings/movie", strSync);
+
+INCLUDE_ASM("asm/nonmatchings/movie", strKickCD);
